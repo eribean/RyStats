3bedc3358c8ad5e8787f0be9fa555f90428c62d6
Ryan S <eribean1@gmail.com>
2021-08-01 14:48:42 -0400

    Made map conform to standard practices instead of ratio.

--- dimensionality/map.py
+++ dimensionality/map.py
@@ -1,52 +1,43 @@
 from functools import partial
 import numpy as np
 
-from RyStats.common import polychoric
-from RyStats.factoranalysis import principal_components_analysis as pca
-
 
 __all__ = ["minimum_average_partial"]
 
 
-def _get_correlation_method(correlation):
-    if correlation[0].lower() == 'pearsons':
-        func = np.corrcoef
-    elif correlation[0].lower() == 'polychoric':
-        func = partial(polychoric.polychoric_matrix, start_val=correlation[1], stop_val=correlation[2])
-    else:
-        raise ValueError('Unknown correlation method {}'.format(correlation[0]))
-        
-    return func
-
-
-def minimum_average_partial(the_data, correlation=('pearsons', )):
-    """ Determines dimensionality based on the matrix of partial correlations.
+def minimum_average_partial(correlation_matrix):
+    """ Determines dimensionality based on partial correlations.
 
     Args:
-        the_data:  Input array with the_data.shape == (n_items, n_observations)
-        correlation: Method to construct correlation matrix either:
-                        ('pearsons',) for continuous data
-                        ('polychoric', min_val, max_val) for ordinal data
-                        min_val and max_val are the range for the ordinal data
+        correlation_matrix:  Input correlation matrix from pearsons 
+                             or polychoric correlations
 
     Returns
-        ratio of partial correlation squared-sum to correlation squared-sum
+        n_factors: number of factors determined
+        metric: squared average partial correlations
+        
+    Note:
+        Based on Velicer's MAP test, https://doi.org/10.1007/BF02293557
     """    
-    func = _get_correlation_method(correlation)
+    n_items = correlation_matrix.shape[0]
+    scalar = 2 / (n_items * (n_items - 1))
     
-    correlation_matrix = func(the_data)
     diag_inds = np.tril_indices_from(correlation_matrix, k=-1)
-    denominator = np.square(correlation_matrix[diag_inds]).sum()
     
-    # Principal Components
-    the_ratio = list()
-    for n_factors in range(1, the_data.shape[0] - 1):
-        facts, _, _ = pca(correlation_matrix, n_factors=n_factors)
+    the_ratio = np.zeros((n_items-1))
+    the_ratio[0] = scalar * np.square(correlation_matrix[diag_inds]).sum()
+
+    # Basic Principal Components
+    u1, s1, _ = np.linalg.svd(correlation_matrix, hermitian=True)
+    s1 = np.sqrt(s1)
+
+    for ndx, n_factors in enumerate(range(1, n_items - 1)):
+        facts = u1[:, :n_factors] * s1[:n_factors]
 
         partial_difference = correlation_matrix - facts @ facts.T
         weighting = 1.0 / np.sqrt(np.diag(partial_difference))
 
         partial_correlation = partial_difference * np.outer(weighting, weighting)
-        the_ratio.append(np.square(partial_correlation[diag_inds]).sum() / denominator)
+        the_ratio[ndx+1] = scalar * np.square(partial_correlation[diag_inds]).sum() 
 
-    return np.argmin(the_ratio) + 1, the_ratio
+    return np.argmin(the_ratio), the_ratio
