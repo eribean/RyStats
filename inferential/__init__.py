8c42fe2babedec5467d9f5f625ac4b1599836abd
Ryan S <eribean1@gmail.com>
2021-08-28 12:50:57 -0400

    Adding unittests.

--- inferential/__init__.py
+++ inferential/__init__.py
@@ -1,3 +1,4 @@
 from .ttests import *
 from .oneway_anova import *
 from .post_hoc import *
+from .twoway_anova import *
---
+++ inferential/test/test_twoway_anova.py
@@ -0,0 +1,21 @@
+import unittest
+
+import numpy as np
+
+from RyStats.inferential import twoway_anova
+
+
+class TestTwoWayAnova(unittest.TestCase):
+    """Test fixture for two way anova."""
+
+    def test_two_way_anova_balanced(self):
+        """Testing a balanced two-way anova."""
+        pass
+
+    def test_two_way_anova_unbalanced(self):
+        """Testing a balanced two-way anova."""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
