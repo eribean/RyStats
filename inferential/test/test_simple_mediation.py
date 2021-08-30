61f532b741753cb635d755f43752bc823ca5db45
Ryan S <eribean1@gmail.com>
2021-08-29 21:28:31 -0400

    Initial commit of simple mediation.

---
+++ inferential/test/test_simple_mediation.py
@@ -0,0 +1,14 @@
+import unittest
+
+import numpy as np
+
+from RyStats.inferential import simple_mediation
+
+
+class TestSimpleMediation(unittest.TestCase):
+    """Test Fixture for Simple Mediation."""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
